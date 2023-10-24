4ab955026e44195410072383f3f70bf2486b3971
Chen-lianyu <65639995+Chen-lianyu@users.noreply.github.com>
2023-10-24 09:24:24 +0800

    Update game.h
    Update GamePackageName

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.tencent.tmgp.sgame"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
